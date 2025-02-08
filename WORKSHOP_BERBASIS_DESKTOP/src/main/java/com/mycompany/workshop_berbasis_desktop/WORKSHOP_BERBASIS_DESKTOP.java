551ce260756e4e11fd874cce2f02dc28d23399d4
Zedd020206 <zaidanalhafidz22@gmail.com>
2025-02-08 13:16:06 +0700

    Create WORKSHOP_BERBASIS_DESKTOP.java

---
+++ WORKSHOP_BERBASIS_DESKTOP/src/main/java/com/mycompany/workshop_berbasis_desktop/WORKSHOP_BERBASIS_DESKTOP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.workshop_berbasis_desktop;
+
+/**
+ *
+ * @author HP
+ */
+public class WORKSHOP_BERBASIS_DESKTOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
