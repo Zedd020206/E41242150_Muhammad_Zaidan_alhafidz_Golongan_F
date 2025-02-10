9a7f4db8d77136c3c5c366d43af43e6305808870
Zedd020206 <zaidanalhafidz22@gmail.com>
2025-02-10 21:32:01 +0700

    Update tugas_No1.java

--- tugasWSIBD_minggu2/src/main/java/com/mycompany/tugaswsibd_minggu2/tugas_No1.java
+++ tugasWSIBD_minggu2/src/main/java/com/mycompany/tugaswsibd_minggu2/tugas_No1.java
@@ -11,6 +11,10 @@ package com.mycompany.tugaswsibd_minggu2;
 public class tugas_No1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello ");
+        System.out.println("World!");
+        
+        System.out.print("Hello");
+        System.out.print("World!");
     }
 }
