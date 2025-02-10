f53cfad447ee35f72326ab6b1e95d732f1d9be77
Zedd020206 <zaidanalhafidz22@gmail.com>
2025-02-10 21:24:37 +0700

    Create tugas_No1.java

---
+++ tugasWSIBD_minggu2/src/main/java/com/mycompany/tugaswsibd_minggu2/tugas_No1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugaswsibd_minggu2;
+
+/**
+ *
+ * @author HP
+ */
+public class tugas_No1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
