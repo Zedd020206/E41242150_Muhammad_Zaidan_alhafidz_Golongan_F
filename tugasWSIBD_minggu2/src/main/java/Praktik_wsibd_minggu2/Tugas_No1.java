681485f8470d9caf75307dc6336c8259c8522d60
Zedd020206 <zaidanalhafidz22@gmail.com>
2025-02-12 01:48:09 +0700

    Create Tugas_No1.java

---
+++ tugasWSIBD_minggu2/src/main/java/Praktik_wsibd_minggu2/Tugas_No1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktik_wsibd_minggu2;
+
+public class Tugas_No1 {
+    public class Program_tampilkan_nama_ID_statusSiswa {
+     public static void main(String[] args) {
+        String fName = "Lisa";
+        String IName = "Palombo";
+        String stuld = "123456789"; 
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name    :" + fName + " " + IName);
+        System.out.println("Student ID      :" + stuld);
+        System.out.println("Student Status  :" + stuStatus);
+    }
+}
+
+    
+}
