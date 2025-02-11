e03da4f2fb73c2958e117f73d2af252aca878abc
Zedd020206 <zaidanalhafidz22@gmail.com>
2025-02-12 02:00:09 +0700

    Create Praktik_No2.java

---
+++ tugasWSIBD_minggu2/src/main/java/Praktik_wsibd_minggu2/Praktik_No2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktik_wsibd_minggu2;
+
+
+public class Praktik_No2 {
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println(nama);
+        System.out.println(a);
+    }
+    
+}
