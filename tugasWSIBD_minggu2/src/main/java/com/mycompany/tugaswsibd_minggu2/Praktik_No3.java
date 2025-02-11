6fb5d11334c0038fc058b7dc6667c76212cfe12b
Zedd020206 <zaidanalhafidz22@gmail.com>
2025-02-11 18:59:28 +0700

    Create Praktik_No3.java

---
+++ tugasWSIBD_minggu2/src/main/java/com/mycompany/tugaswsibd_minggu2/Praktik_No3.java
@@ -0,0 +1,28 @@
+package com.mycompany.tugaswsibd_minggu2;
+
+public class Praktik_No3 {
+    static class book {
+        int price;
+        int pages;
+        
+        public void set (int price, int pages) {
+            this.price = price;
+            this.pages = pages;
+            
+        }
+        public void show (){
+            System.out.println("book information ");
+            System.out.println("book price      : " +price);
+            System.out.println("number of pages : " +pages);
+        }
+    }
+   
+            public static void main(String[] args) {
+            book javabook = new book();
+            javabook.set(60000, 100);
+            javabook.show();
+            
+        }
+    }
+    
+        
