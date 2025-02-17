d40fd1f1078702e3afda8cd4b0e67332a0fecf7d
Zedd020206 <zaidanalhafidz22@gmail.com>
2025-02-17 14:46:35 +0700

    Create Praktik_No2.java

---
+++ WSIBD/src/main/java/Praktik_wsibd_minggu3_hari1/Praktik_No2.java
@@ -0,0 +1,11 @@
+package Praktik_wsibd_minggu3_hari1;
+
+public class Praktik_No2 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+    }
+}
